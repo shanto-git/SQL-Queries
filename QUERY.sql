e8a202855b02c4a17e23c7678849d561dc3b8a5e
Masuduzzaman <md.santo0583210@gmail.com>
2026-06-16 11:38:13 +0600

    add query-2/3

--- QUERY.sql
+++ QUERY.sql
@@ -109,3 +109,18 @@ SELECT match_id, fixture, base_ticket_price
 FROM Matches 
 WHERE tournament_category = 'Champions League' 
   AND match_status = 'Available';
+
+
+
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+
+SELECT user_id, full_name, email
+FROM Users 
+WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%'
+
+
+-- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+
+SELECT booking_id, user_id, match_id, 
+COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM Bookings WHERE payment_status IS NULL;
